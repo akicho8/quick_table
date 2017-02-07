69f687824f1bc7abd22bff5066520f49f60a5f5d
akicho8 <akicho8@gmail.com>
2017-02-07 14:07:03 +0900

    Rails が定義されているときだけ engine.rb を読み込む

--- lib/quick_table.rb
+++ lib/quick_table.rb
@@ -1,6 +1,6 @@
 require "quick_table/quick_table"
 require "quick_table/core_ext"
-require "quick_table/engine"
+require "quick_table/engine" if defined?(Rails)
 require "quick_table/version"
 
 module QuickTable
